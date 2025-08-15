fc47decb0def3de0d8b5b3ee44dfb88a03033466
Ashp116 <ashp116@outlook.com>
2025-08-15 17:49:58 -0400

    INIT: Initial commit

--- Platforms/iOS/AppDelegate.cs
+++ Platforms/iOS/AppDelegate.cs
@@ -5,5 +5,5 @@ namespace APKDrop;
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
